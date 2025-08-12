20851fe1eedccddc7eea3663bb2b6a49e8167a10
Tai Ngo <tdng71414@edu.ucl.dk>
2025-08-12 13:06:20 +0200

    added comments

--- WpfCardGame/WpfCardGame/Model/Card.cs
+++ WpfCardGame/WpfCardGame/Model/Card.cs
@@ -7,38 +7,52 @@ using System.Threading.Tasks;
 namespace WpfCardGame.Model {
     public class Card {
         public int Id { get; set; }
-        A1 + B1
-        A2 + B2
-        A3 + B3
-        A4 + B4
-        A5 + B5
-        A6 + B6
-        A7 + B7
-        A8 + B8 
+        //A1 + B1
+        //A2 + B2
+        //A3 + B3
+        //A4 + B4
+        //A5 + B5
+        //A6 + B6
+        //A7 + B7
+        //A8 + B8 
 
 
-        public string PairKey { get; set; }
-        1, 2, 3, 4
+        public string PairKey
+        {
+            get;
+            set
+            {
+                if (value != "0") { throw new Exception(); }
+                else PairKey = value;
+            }
+        }
 
+        // svarer til symbol i opgaven
+        // fra 1 til 8, da der er 16 kort (8 par)        
 
+        
 
         public bool IsFlipped { get; set; }
-        Kan ikke trykkes på når IsFlipped = True;
-        Kan trykkes på når IsFlipped = False
-        Afventer et andet korts Isflipped = true
-            Hvis 2 kort er Isflipped = true,
-            1: Tjek om IsMatched
-            2: Ellers Break, og sæt IsFlipped til False.
-
-
+        //Kan ikke trykkes på når IsFlipped = True;
+        //Kan trykkes på når IsFlipped = False
+        //Afventer et andet korts Isflipped = true
+        //    Hvis 2 kort er Isflipped = true,
+        //    1: Tjek om IsMatched
+        //    2: Ellers Break, og sæt IsFlipped til False.
 
 
 
         public bool IsMatched { get; set; }
-        first.PairKey == second.PairKey
-            Deaktivér de tilhørende ID's
-            Tillæg point til spilleren - Samt registrér hvilket PairKey der blev fundet.
-
-
+        //first.PairKey == second.PairKey
+        //    Deaktivér de tilhørende ID's
+        //    Tillæg point til spilleren - Samt registrér hvilket PairKey der blev fundet.
+        
+        public Card (int Id, string PairKey)
+        {
+            this.Id = Id;
+            this.PairKey = PairKey;
+            this.IsFlipped = false;
+            this.IsMatched = false;
+        }
     }
 }
